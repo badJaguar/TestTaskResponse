2955cd9f19ad9d782abbf41f34c6b35e0303204d
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2018-10-12 15:41:23 +0300

    First commit

--- TestTaskResponse/Program.cs
+++ TestTaskResponse/Program.cs
@@ -6,16 +6,78 @@ using System.Threading.Tasks;
 
 namespace TestTaskResponse
 {
+    /// <summary>
+    /// The class represents a uniquely named task and a list of dependencies for it.
+    /// Dependency is a task that has to be finished before the current task may be started.
+    /// </summary>
+    public class Task
+    {
+        /// <summary>
+        /// Unique name of the task
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Array of task dependencies
+        /// </summary>
+        public string[] Dependencies { get; }
+
+        public Task(string name, params string[] dependencies)
+        {
+            Name = name;
+            Dependencies = dependencies;
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // The following array is an example of specific tasks and dependencies between them.
+            // For example the following constructor:
+            // new Task("task_a", "task_c")
+            // means that task_a may be started only after task_c is complete
+            var tasks = new[]
+            {
+                new Task("task_a", "task_c"),
+                new Task("task_b", "task_c"),
+                new Task("task_c", "task_e"),
+                new Task("task_d", "task_a", "task_e"),
+                new Task("task_e"),
+            };
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            var sortedTasks = Sort(tasks);
+            foreach (var task in sortedTasks)
+            {
+                Console.WriteLine($"- {task.Name} {task.Dependencies.Length}");
+            }
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// <example></example>
+        /// Implement a function that accepts a list of tasks (and their dependencies) as an input
+        /// and produces a list of tasks in the correct order (the order in which they may be executed)
+        ///
+        /// For example if we provide the following input
+        /// var tasks = new Task[]
+        /// {
+        ///     new Task("task_a", "task_b", "task_c"),
+        ///     new Task("task_b"),
+        ///     new Task("task_c", "task_b"),
+        /// }
+        /// 
+        /// the output shall be:
+        /// - task_b
+        /// - task_c
+        /// - task_a
+        /// </summary>
+        public static Task[] Sort(Task[] tasks)
+        {
+            return tasks;
+            //var sortingTasks = tasks.Where(t => t.Dependencies.SequenceEqual(t.Dependencies)).OrderBy(t => t.Name);
+            //return sortingTasks.Reverse();
         }
     }
 }
